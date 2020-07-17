45f1a39b2db65a7a8ba14748232599462206e272
KennethSC <kennethsantacruz12@gmail.com>
2020-07-17 15:18:51 -0700

    Refined Unit Tests

--- Red_Black_Tree/RB_Tree_Test.c
+++ Red_Black_Tree/RB_Tree_Test.c
@@ -2,29 +2,24 @@
 #include<stdio.h>
 #include<assert.h>
 #include<string.h>
-
 #include "RB_Tree.h"
 
 int points = 0;
 int numTests = 10;
 
-
 // Correct = 0
-int empty_test()
+int empty_test(Tree T)
 {
-    Tree T = newTree(0);
     int ans;
-    
     ans = size(T);
-    freeTree(&T);
-    return ans;
 
+    return ans;
 }
 
+
 // Correct = -1
-int emptyTraverse_test()
+int emptyTraverse_test(Tree T)
 {
-    Tree T = newTree(0);
     int ans = -1;
 
     if (beginForward(T) != VAL_UNDEF) ans =  1;
@@ -32,19 +27,14 @@ int emptyTraverse_test()
     if (currentVal(T) != VAL_UNDEF) ans =  3;
     if (currentKey(T) != KEY_UNDEF) ans = 4;
 
-    freeTree(&T);
     return ans;
-
 }
 
-// Correct = 6
-int insertSize_test()
-{
-    Tree T = newTree(0);
-    int ans;
 
-    int* p = calloc(10, sizeof(int));
-    for(int x = 0; x < 10; x++) p[x] = x;
+// Correct = 6
+int insertSize_test(Tree T, int* p)
+{
+    int ans;
 
     insert(T, "P", &p[0]);
     insert(T, "Y", &p[0]);
@@ -54,20 +44,16 @@ int insertSize_test()
     insert(T, "N", &p[0]);
 
     ans = size(T);
-    freeTree(&T);
-    free(p);
+    makeEmpty(T);
+
     return ans;
 }
 
 
 // Correct = 3
-int deleteSize_test()
+int deleteSize_test(Tree T, int* p)
 {
-    Tree T = newTree(0);
     int ans;
-
-    int* p = calloc(10, sizeof(int));
-    for(int x = 0; x < 10; x++) p[x] = x;
 
     insert(T, "a", &p[0]);
     insert(T, "b", &p[0]);
@@ -87,21 +73,16 @@ int deleteSize_test()
     delete(T, "z");
 
     ans = size(T);
-    freeTree(&T);
-    free(p);
-    return ans;
+    makeEmpty(T);
 
+    return ans;
 }
 
 
 // Correct = "x"
-char* currKey_test()
+char* currKey_test(Tree T, int* p)
 {
-    Tree T = newTree(0);
     char* ans;
-
-    int* p = calloc(10, sizeof(int));
-    for(int x = 0; x < 10; x++) p[x] = x;
 
     insert(T, "l", &p[0]);
     insert(T, "m", &p[0]);
@@ -126,21 +107,16 @@ char* currKey_test()
     }
 
     ans = currentKey(T);
-    freeTree(&T);
-    free(p);
-    return ans;
+    makeEmpty(T);
 
+    return ans;
 }
 
 
 // Correct = "e"
-char* prev_test()
+char* prev_test(Tree T, int* p)
 {
-    Tree T = newTree(0);
     char* ans;
-
-    int* p = calloc(10, sizeof(int));
-    for(int x = 0; x < 10; x++) p[x] = x;
 
     insert(T, "k", &p[0]);
     insert(T, "e", &p[0]);
@@ -165,21 +141,16 @@ char* prev_test()
     prev(T);
 
     ans = currentKey(T);
-    freeTree(&T);
-    free(p);
-    return ans;
+    makeEmpty(T);
 
+    return ans;
 }
 
 
 // Correct = 9
-int lookup_test()
+int lookup_test(Tree T, int* p)
 {
-    Tree T = newTree(0);
     int ans;
-
-    int* p = calloc(10, sizeof(int));
-    for(int x = 0; x < 10; x++) p[x] = x;
 
     insert(T, "P", &p[0]);
     insert(T, "Y", &p[4]);
@@ -189,20 +160,16 @@ int lookup_test()
     insert(T, "N", &p[0]);
 
     ans = *lookup(T, "O");
-    freeTree(&T);
-    free(p);
-    return ans;
+    makeEmpty(T);
 
+    return ans;
 }
 
-// Correct = 0
-int makeEmpty_test()
-{
-    Tree T = newTree(0);
-    int ans;
 
-    int* p = calloc(10, sizeof(int));
-    for(int x = 0; x < 10; x++) p[x] = x;
+// Correct = 0
+int makeEmpty_test(Tree T, int* p)
+{
+    int ans;
 
     insert(T, "m", &p[0]);
     insert(T, "d", &p[0]);
@@ -217,21 +184,17 @@ int makeEmpty_test()
     makeEmpty(T);
 
     ans = size(T);
-    freeTree(&T);
-    free(p);
-    return ans;
+    makeEmpty(T);
 
+    return ans;
 }
 
+
 // Correct = 6
-int insertVal_test()
+int insertVal_test(Tree T, int* p)
 {
-    Tree T = newTree(0);
     int ans;
     int wrong;
-
-    int* p = calloc(10, sizeof(int));
-    for(int x = 0; x < 10; x++) p[x] = x;
 
     insert(T, "m", &p[0]);
     insert(T, "n", &p[1]);
@@ -246,8 +209,8 @@ int insertVal_test()
     if(*currentVal(T) != p[2])
     {
         wrong = *currentVal(T);
-        freeTree(&T);
-        free(p);
+	    makeEmpty(T);
+
         return wrong;
     }
 
@@ -256,28 +219,23 @@ int insertVal_test()
     if(*currentVal(T) != p[6])
     {
         wrong = *currentVal(T);
-        freeTree(&T);
-        free(p);
+     	makeEmpty(T);
+
         return wrong;
     }
 
     ans = *currentVal(T);
-    freeTree(&T);
-    free(p);
-    return ans;
+    makeEmpty(T);
 
+    return ans;
 }
 
+
 // Correct = 4
-int deleteVal_test()
+int deleteVal_test(Tree T, int* p)
 {
-    Tree T = newTree(0);
     int ans;
     int wrong;
-
-    int* p = calloc(10, sizeof(int));
-    for(int x = 0; x < 10; x++) p[x] = x;
-
 
     insert(T, "m", &p[0]);
     insert(T, "n", &p[1]);
@@ -297,8 +255,7 @@ int deleteVal_test()
     if(*currentVal(T) != p[6])
     {
         wrong = *currentVal(T);
-        freeTree(&T);
-        free(p);
+        makeEmpty(T);
         return wrong;
     }
 
@@ -309,16 +266,14 @@ int deleteVal_test()
     if(*currentVal(T) != p[4])
     {
         wrong = *currentVal(T);
-        freeTree(&T);
-        free(p);
+	    makeEmpty(T);
         return wrong;
     }
 
     ans = *currentVal(T);
-    freeTree(&T);
-    free(p);
-    return ans;
+    makeEmpty(T);
 
+    return ans;
 }
 
 
@@ -326,46 +281,57 @@ void test(int expected, int actual, char* test_name)
 {
     if(expected == actual)
     {
+
         printf("\n%s: PASSED", test_name);
-        points++;
+	    points++;
     }
     else
     {
+
         printf("\n%s: FAILED (expected = %d, actual = %d)", test_name, expected, actual);
     }
-
 }
+
 
 void testChar(char* expected, char* actual, char* test_name)
 {
     if(strcmp(expected, actual) == 0)
     {
+
         printf("\n%s: PASSED", test_name);
-        points++;
+	    points++;
     }
     else
     {
+
         printf("\n%s: FAILED (expected = %s, actual = %s)", test_name, expected, actual);
     }
-
 }
 
 
 int main()
 {
-    testChar("e", prev_test(), "prev_test");
-    test(0, empty_test(), "empty_test");
-    test(9, lookup_test(), "lookup_test");
-    testChar("x", currKey_test(), "currKey_test");
-    test(6, insertVal_test(), "insertVal_test");
-    test(4, deleteVal_test(), "deleteVal_test");
-    test(0, makeEmpty_test(), "makeEmpty_test");
-    test(6, insertSize_test(), "insertSize_test");
-    test(3, deleteSize_test(), "deleteSize_test");
-    test(-1, emptyTraverse_test(), "emptyTraverse_test");
+    Tree T = newTree(0);
+    int* p = calloc(10, sizeof(int));
+    
+    for(int i = 0; i < 10; i++) p[i] = i;  
+
+    testChar("e", prev_test(T, p), "prev_test");
+    test(0, empty_test(T), "empty_test");
+    test(9, lookup_test(T, p), "lookup_test");
+    testChar("x", currKey_test(T, p), "currKey_test");
+    test(6, insertVal_test(T, p), "insertVal_test");
+    test(4, deleteVal_test(T, p), "deleteVal_test");
+    test(0, makeEmpty_test(T, p), "makeEmpty_test");
+    test(6, insertSize_test(T, p), "insertSize_test");
+    test(3, deleteSize_test(T, p), "deleteSize_test");
+    test(-1, emptyTraverse_test(T), "emptyTraverse_test");
 
     printf("\n\nYou passed %d out of %d tests", points, numTests);
     printf("\n\n");
+
+    freeTree(&T);
+    free(p);
 
     return 0;
 }
